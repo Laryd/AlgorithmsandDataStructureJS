52957b37e16d9f47a19968535ebd824c2db0cc6c
Laryd <hillarymuboka@gmail.com>
2023-02-16 16:58:57 +0300

    binary search tree

--- algosjavascript.js
+++ algosjavascript.js
@@ -310,4 +310,11 @@ class BST {
   constructor(){
     this.root = null
   }
+  add(data){
+    const node = this.root;
+    if(node === null){
+      this.root = new Node(data)
+      return
+    }
+  }
 }
